c7ae7d91f82c820b1e671e5ccebd440a5ab2e360
Yomar Miranda <mirandayomar@gmail.com>
2018-10-14 21:16:14 -0500

    Show selected video in modal.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { YoutubeService } from '../../services/youtube.service';
 
+declare let $: any;
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -8,14 +10,26 @@ import { YoutubeService } from '../../services/youtube.service';
 })
 export class HomeComponent implements OnInit {
   videos: any[] = [];
+  videoSelected: any;
 
   constructor(public _youtube: YoutubeService) {
     this._youtube.getVideos().subscribe((response => {
       this.videos = response;
+      console.log(this.videos);
     }));
   }
 
   ngOnInit() {
+  }
+
+  viewVideo(video: any) {
+    this.videoSelected = video;
+    $('#exampleModal').modal();
+  }
+
+  closeVideo() {
+    this.videoSelected = null;
+    $('#exampleModal').modal('hide');
   }
 
 }
